fe1034644d88d4b576d35033a953703e1f5eda52
Anton Volkov <akronbolkov@gmail.com>
2017-07-06 19:29:59 +0300

    feat: added ScrollToTop component for React Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import ScrollToTop from './Components/ScrollToTop';
 import Header from './Header';
 import Footer from './Footer';
 import Complexes from './Complexes/List';
@@ -9,12 +10,14 @@ import Complex from './Complexes/Show';
 
 export default () =>
   (<Router>
-    <div className="App">
-      <Header />
+    <ScrollToTop>
+      <div className="App">
+        <Header />
 
-      <Route exact path="/" component={Complexes} />
-      <Route path="/complexes/:id" component={Complex} />
+        <Route exact path="/" component={Complexes} />
+        <Route path="/complexes/:id" component={Complex} />
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </ScrollToTop>
   </Router>);
