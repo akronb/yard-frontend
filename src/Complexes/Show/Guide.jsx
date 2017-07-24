b68cbe144c74b57dea74a3a05085a6a1ad9bdc3f
akronb <akronbolkov@gmail.com>
2017-07-24 19:43:37 +0300

    feat: adaptive Guide

--- src/Complexes/Show/Guide.jsx
+++ src/Complexes/Show/Guide.jsx
@@ -4,46 +4,79 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+import { media } from '../../utils/styles';
 import arrowImg from '../../img/arrow-blue.svg';
 import Image from '../../Components/Image';
 
 const PUBLIC_URL: string = process.env.PUBLIC_URL || '';
 
 const Wrapper = styled.div`
-  padding-top: 2rem;
-  padding-bottom: 13.625rem;
   background-color: #3e4247;
+
+  ${media.tablet`
+    padding-top: 2rem;
+    padding-bottom: 13.625rem;
+  `};
 `;
 
 const District = styled.div`
   font-family: "Philosopher", serif;
-  font-size: 1.5rem;
+  font-size: 1.25rem;
   font-weight: 700;
-  line-height: 27px;
+  line-height: 22px;
   color: #a9afb6;
-  margin-bottom: 3rem;
+  margin: 3rem 1rem 2rem;
+
+  ${media.tablet`
+    margin: 0px 0px 2rem;
+  `};
+
+  ${media.desktop`
+    font-size: 1.5rem;
+    line-height: 27px;
+    margin: 0px 0px 3rem;
+  `};
 `;
 
 const Title = styled.div`
   font-family: "Philosopher", serif;
-  font-size: 3rem;
+  font-size: 2rem;
   font-weight: bold;
-  line-height: 60px;
+  line-height: 44px;
   color: #ffffff;
+  margin: 0px 1rem;
+
+  ${media.tablet`
+    margin: 0px;
+  `};
+
+  ${media.desktop`
+    font-size: 3rem;
+    line-height: 60px;
+  `};
 `;
 
 const GuideLink = styled(Link)`
   display: inline-block;
   text-decoration: none;
-  font-size: 16px;
+  font-size: 1rem;
   text-align: left;
   line-height: 24px;
   color: #00779a;
-  margin-top: 3rem;
+  margin: 2rem 1rem 3rem;
+
   &:after {
     content: url(${arrowImg});
     margin-left: .5rem;
   }
+
+  ${media.tablet`
+    margin: 2rem 0px 0px;
+  `};
+
+  ${media.desktop`
+    margin: 3rem 0px 0px;
+  `};
 `;
 
 const Photo = styled(Image)`
@@ -53,15 +86,13 @@ const Photo = styled(Image)`
 export default () =>
   (<Wrapper>
     <Grid>
-      <Row middle="lg">
-        <Col lg={6}>
+      <Row middle="sm">
+        <Col sm={6} xs={12} last="xs" first="sm">
           <District>Якиманка</District>
-          <Title>
-            Исторический центр Москвы в двух километрах&nbsp;от Кремля
-          </Title>
+          <Title>Исторический центр Москвы в двух километрах&nbsp;от Кремля</Title>
           <GuideLink to="#">Гид по Якиманке</GuideLink>
         </Col>
-        <Col lg={6}>
+        <Col sm={6} xs={12}>
           <Photo
             x1={`${PUBLIC_URL}/img/polyanka-photo.jpg`}
             x2={`${PUBLIC_URL}/img/polyanka-photo@2x.jpg`}
