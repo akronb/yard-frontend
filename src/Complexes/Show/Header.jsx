e9a785540cc7f2a0143db7f7354e02345dcda708
akronb <akronbolkov@gmail.com>
2017-07-24 19:41:49 +0300

    feat: adaptive Header

--- src/Complexes/Show/Header.jsx
+++ src/Complexes/Show/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Grid } from 'react-flexbox-grid';
 import styled from 'styled-components';
 
+import { media } from '../../utils/styles';
 import type { LocationType } from '../types';
 
 const Wrapper = styled.div`
@@ -14,9 +15,13 @@ const Name = styled.h1`
   margin: 0px;
   font-family: "Philosopher", serif;
   font-weight: 700;
-  font-size: 2rem;
+  font-size: 1.625rem;
   line-height: 36px;
   color: #3e4247;
+
+  ${media.tablet`
+    font-size: 2rem;
+  `};
 `;
 
 const Location = styled.p`
@@ -29,6 +34,7 @@ const Location = styled.p`
 `;
 
 const Button = styled.button`
+  display: none;
   align-self: flex-start;
   cursor: pointer;
   background-color: transparent;
@@ -40,6 +46,10 @@ const Button = styled.button`
   font-weight: 300;
   font-size: .625rem;
   line-height: .625rem;
+
+  ${media.tablet`
+    display: block;
+  `};
 `;
 
 function formatLocation({ subLocalityName, street, postalCode }: LocationType): string {
