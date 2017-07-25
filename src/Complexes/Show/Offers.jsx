8488562df2156e286edf8d0ad2cab5fd84706d75
akronb <akronbolkov@gmail.com>
2017-07-25 19:26:43 +0300

    feat: adaptive Offers

--- src/Complexes/Show/Offers.jsx
+++ src/Complexes/Show/Offers.jsx
@@ -1,23 +1,58 @@
 // @flow
 import React from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Grid } from 'react-flexbox-grid';
 import styled from 'styled-components';
 
+import { media } from '../../utils/styles';
 import Heading from './Heading';
 
 const Wrapper = styled.div`
   background-color: #f4f5f9;
-  padding: 4rem 0px;
+  padding: 2rem 0px;
+
+  ${media.tablet`
+    padding: 4rem 0px;
+  `};
 `;
 
 const Title = styled(Heading)`
-  text-align: center;
+  text-align: left;
+  margin-left: 1rem;
+  margin-right: 1rem;
   margin-bottom: 1.5rem;
+
+  ${media.tablet`
+    text-align: center;
+    margin-left: 0px;
+    margin-right: 0px;
+  `};
+`;
+
+const Row = styled.div`
+  display: flex;
+  overflow-x: auto;
+  padding: 0px 1rem;
+
+  ${media.desktop`
+    overflow-x: hidden;
+    padding: 0px;
+  `};
 `;
 
 const Card = styled.div`
-  padding: 1.5rem 2rem;
+  flex: 1 0 320px;
+  padding: 1.5rem 1.625rem;
   background-color: #fff;
+
+  &:not(:last-child) {
+    margin-right: 1rem;
+  }
+
+  ${media.desktop`
+    flex: 1 0 0;
+    max-width: 390px;
+    padding: 1.5rem 2rem;
+  `};
 `;
 
 const CardTitle = styled.h3`
@@ -53,9 +88,13 @@ const Button = styled.button`
   border: none;
   border-radius: 2px;
   color: #fff;
-  padding: .75rem 2rem;
+  padding: .75rem 1.5rem;
   font-family: 'Fira Sans', sans-serif;
   font-size: 1rem;
+
+  ${media.desktop`
+    padding: .75rem 2rem;
+  `};
 `;
 
 type Props = {
@@ -66,57 +105,51 @@ export default (props: Props) =>
   (<Wrapper>
     <Grid>
       <Title>
-        Предложения в {props.name}
+        Предложения в ЖК «{props.name}»
       </Title>
       <Row>
-        <Col lg={4}>
-          <Card>
-            <CardTitle>1-комнатные квартиры</CardTitle>
-            <Feature>
-              <Label>Площадь</Label>
-              <Value>от 59 до 120 м²</Value>
-            </Feature>
-            <Feature>
-              <Label>Стоимость</Label>
-              <Value>от 20.3 до 84.2 млн руб</Value>
-            </Feature>
-            <ButtonWrapper>
-              <Button>Посмотреть предложения</Button>
-            </ButtonWrapper>
-          </Card>
-        </Col>
-        <Col lg={4}>
-          <Card>
-            <CardTitle>2-комнатные квартиры</CardTitle>
-            <Feature>
-              <Label>Площадь</Label>
-              <Value>от 59 до 120 м²</Value>
-            </Feature>
-            <Feature>
-              <Label>Стоимость</Label>
-              <Value>от 20.3 до 84.2 млн руб</Value>
-            </Feature>
-            <ButtonWrapper>
-              <Button>Посмотреть предложения</Button>
-            </ButtonWrapper>
-          </Card>
-        </Col>
-        <Col lg={4}>
-          <Card>
-            <CardTitle>3-комнатные квартиры</CardTitle>
-            <Feature>
-              <Label>Площадь</Label>
-              <Value>от 59 до 120 м²</Value>
-            </Feature>
-            <Feature>
-              <Label>Стоимость</Label>
-              <Value>от 20.3 до 84.2 млн руб</Value>
-            </Feature>
-            <ButtonWrapper>
-              <Button>Посмотреть предложения</Button>
-            </ButtonWrapper>
-          </Card>
-        </Col>
+        <Card>
+          <CardTitle>1-комнатные квартиры</CardTitle>
+          <Feature>
+            <Label>Площадь</Label>
+            <Value>от 59 до 120 м²</Value>
+          </Feature>
+          <Feature>
+            <Label>Стоимость</Label>
+            <Value>от 20.3 до 84.2 млн руб</Value>
+          </Feature>
+          <ButtonWrapper>
+            <Button>Посмотреть предложения</Button>
+          </ButtonWrapper>
+        </Card>
+        <Card>
+          <CardTitle>2-комнатные квартиры</CardTitle>
+          <Feature>
+            <Label>Площадь</Label>
+            <Value>от 59 до 120 м²</Value>
+          </Feature>
+          <Feature>
+            <Label>Стоимость</Label>
+            <Value>от 20.3 до 84.2 млн руб</Value>
+          </Feature>
+          <ButtonWrapper>
+            <Button>Посмотреть предложения</Button>
+          </ButtonWrapper>
+        </Card>
+        <Card>
+          <CardTitle>3-комнатные квартиры</CardTitle>
+          <Feature>
+            <Label>Площадь</Label>
+            <Value>от 59 до 120 м²</Value>
+          </Feature>
+          <Feature>
+            <Label>Стоимость</Label>
+            <Value>от 20.3 до 84.2 млн руб</Value>
+          </Feature>
+          <ButtonWrapper>
+            <Button>Посмотреть предложения</Button>
+          </ButtonWrapper>
+        </Card>
       </Row>
     </Grid>
   </Wrapper>);
