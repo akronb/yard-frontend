b3a76915e97faa485e1cebdb54057de37f8a7fd6
akronb <akronbolkov@gmail.com>
2017-07-26 13:40:44 +0300

    feat: adaptive Infrastructure

--- src/Complexes/Show/Infrastructure.jsx
+++ src/Complexes/Show/Infrastructure.jsx
@@ -1,8 +1,9 @@
 // @flow
 import React from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Grid } from 'react-flexbox-grid';
 import styled from 'styled-components';
 
+import { media } from '../../utils/styles';
 import Heading from './Heading';
 
 const Wrapper = styled.div`
@@ -10,10 +11,31 @@ const Wrapper = styled.div`
   margin-bottom: 2.5rem;
 `;
 
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const Text = styled.p`
+  box-sizing: border-box;
   margin-top: .5rem;
   margin-bottom: 0px;
   color: #3e4247;
+  padding-right: 1.5rem;
+
+  ${media.tablet`
+    flex: 0 0 25%;`};
+
+  ${media.desktop`
+    padding-left: .5rem;
+    padding-right: .5rem;
+    flex: 0 0 16.66666667%;
+    &:nth-child(6n + 1) {
+      padding-left: 0px;
+    }
+    &:nth-child(6n) {
+      padding-right: 0px;
+    }`};
 `;
 
 type Props = {
@@ -28,12 +50,19 @@ export default (props: Props) => {
       <Heading>Инфраструктура</Heading>
       <Wrapper>
         <Row>
+          <Text>Бассейн</Text>
+          <Text>Детский сад</Text>
+          <Text>Детский сад</Text>
+          <Text>Частная школа</Text>
+          <Text>Бассейн</Text>
+          <Text>Частная школа</Text>
+          <Text>Бассейн</Text>
+          <Text>Детский сад</Text>
+          <Text>Частная школа</Text>
           {amenities.map(amenity =>
-            (<Col lg={2}>
-              <Text>
-                {amenity}
-              </Text>
-            </Col>),
+            (<Text>
+              {amenity}
+            </Text>),
           )}
         </Row>
       </Wrapper>
